b0aca57cf6ca267833e33580731bc9232f3e5eff
Madhukesh <madhukesh04@gmail.com>
2019-09-24 10:48:36 +0530

    Toast with duration and gravity

---
+++ lib/fluttertoast.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
+
+class FlutterToast extends StatefulWidget {
+  @override
+  _FlutterToastState createState() => _FlutterToastState();
+}
+
+class _FlutterToastState extends State<FlutterToast> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Toast"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              onPressed: () => showToast("Show Short Toast"),
+              child: Text(
+                'Show Short Toast',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.blue,
+            ),
+            FlatButton(
+              onPressed: () => showToast("Show Long Toast",
+                  duration: Toast.LENGTH_LONG),
+              child: Text(
+                'Show Long Toast',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.blue,
+            ),
+             FlatButton(
+              onPressed: () => showToast("Show Toast at Bottom",
+                  duration: Toast.LENGTH_LONG,gravity: Toast.BOTTOM),
+              child: Text(
+                'Show Toast at Bottom',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void showToast(String msg, {int duration, int gravity}) {
+    Toast.show(msg, context, duration: duration, gravity: gravity);
+  }
+}
